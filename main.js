421088923c4a78526d1038319a454eb83f5953b5
ReOsK <reoskk91@gmail.com>
2026-01-01 21:01:28 +1100

    Updates

--- main.js
+++ main.js
@@ -1,8 +1,20 @@
+// Dark Mode
+const content = document.getElementById("bodyContent");
+const mode = document.getElementById("mode");
 
-// get 
-const content = document.getElementById('bodyContent');
-const mode = document.getElementById('mode');
+mode.addEventListener("click", () => {
+  content.classList.toggle("dark-mode");
+});
 
-mode.addEventListener('click', () => {
-    content.classList.toggle('dark-mode')
+// Appear Sub Card Info
+const subCards = document.querySelectorAll(".sub-card");
+
+subCards.forEach((card) => {
+  card.addEventListener("click", () => {
+    const info = card.querySelector(".sub-card-info");
+
+    if (info) {
+      info.classList.toggle("appear");
+    }
+  });
 });
